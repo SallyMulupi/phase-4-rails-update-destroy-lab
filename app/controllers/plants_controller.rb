c489c80189fe9741050672c39aa337ce226a1d57
SallyMulupi <mulupisally2gmail.com>
2022-09-30 23:20:46 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,30 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+   # PATCH /plants/:id
+   def update
+    plant = Plant.find(params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+   # DELETE /plants/:id
+   def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    render json:{}
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
